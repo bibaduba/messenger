d29a131ccd4d2aa497e8a2ad4d1455ec523076ba
Daniil.Rastorguyev <danrast@mail.ru>
2023-05-28 21:38:28 +0300

    feat: minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import ToasterContext from './context/ToasterContext'
 import AuthContext from './context/AuthContext'
 
-
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
