65ad4ae8bb6bbabb431d9c37ccc6fd01b05144ee
Daniil Rastorguyev <danrast@mail.ru>
2023-05-28 17:20:52 +0300

    feat: react-hot-toaster install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import './globals.css'
+import ToasterContext from './context/ToasterContext'
+
 import { Inter } from 'next/font/google'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <ToasterContext />
+        {children}
+      </body>
     </html>
   )
 }
